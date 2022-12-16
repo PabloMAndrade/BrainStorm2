48a90ffc2c384f5c9d655f11fd674620fd8542f9
PabloMAndrade <77213572+PabloMAndrade@users.noreply.github.com>
2022-12-16 17:25:18 -0300

    Add files via upload

---
+++ Learning3/assets/js/main.js
@@ -0,0 +1,41 @@
+
+$('.customer-logos').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplayspeed: 1500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
